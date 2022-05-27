7e43ce35352e05e27f0ce6a8ba974ddb1e8ace4c
unknown <enzoroldan2001@gmail.com>
2022-05-27 20:48:52 -0300

    first commit

---
+++ src/pages/AdministrarPacientes.jsx
@@ -0,0 +1,25 @@
+import ListadosPacientes from "../components/ListadoPacientes"
+import Formulario from "../components/Formulario"
+import { useState } from "react"
+
+const AdministrarPacientes = () => {
+  const [mostrarFormulario, setMostrarFormulario] = useState(false)
+  return (
+    <div className="flex flex-col md:flex-row">
+        <button
+          type="button"
+          className="bg-indigo-600 text-white font-bold uppercase mx-10 p-3 rounded-md md:hidden"
+          onClick={ () => { setMostrarFormulario(!mostrarFormulario) } }
+        >{mostrarFormulario ? 'Ocultar Formulario' : 'Mostrar Formulario' } </button>
+        <div className={`${mostrarFormulario ? 'block' : 'hidden '} md:block md:w-1/2 lg:w-2/5`}>
+            <Formulario />            
+        </div>
+
+        <div className="md:w-1/2 lg:w-3/5">
+            <ListadosPacientes />
+        </div>
+    </div>
+  )
+}
+
+export default AdministrarPacientes
